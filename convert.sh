1efb50c62db93f43fb1f574f0aaa7efe92020c47
D. Lisin <mrlisdim@gmail.com>
2022-10-25 03:38:44 +0400

    [convert] FIX: Wrong name of input html file

--- convert.sh
+++ convert.sh
@@ -1,10 +1,19 @@
+# Name of file must be "index.html"
+TEMP_DIR=/tmp/pdf_conv_$RANDOM
+mkdir -p $TEMP_DIR
+cp $1 $TEMP_DIR/index.html
+
+# Convert
 curl --request POST 'https://demo.gotenberg.dev/forms/chromium/convert/html' \
-    --form 'files=@"'$1'"' \
+    --form 'files=@"'$TEMP_DIR/index.html'"' \
     --form 'emulatedMediaType="screen"' \
     --form 'paperWidth="10"' \
-    --form 'paperHeight="13.7"' \
+    --form 'paperHeight="13.8"' \
     --form 'scale="0.8"' \
-    --form 'marginLeft="0"' \
-    --form 'marginRight="0"' \
+    --form 'marginLeft="0.1"' \
+    --form 'marginRight="0.1"' \
     --form 'marginBottom="0.1"' \
+    --form 'marginTop="0.1"' \
     -o $2
+
+rm -rf $TEMP_DIR
